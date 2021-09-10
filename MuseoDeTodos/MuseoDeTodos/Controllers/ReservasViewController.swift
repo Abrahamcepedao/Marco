03462f11269d66fd2f05ec0169b7eea250f0f62e
Abrahamcepedao <a00827666@itesm.mx>
2021-09-09 22:24:30 -0500

    Added tableView to servationns

---
+++ MuseoDeTodos/MuseoDeTodos/Controllers/ReservasViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ReservasViewController.swift
+//  MuseoDeTodos
+//
+//  Created by Abraham Cepeda Oseguera on 09/09/21.
+//
+
+import UIKit
+
+class ReservasViewController: UIViewController {
+
+    @IBOutlet weak var reservasTV: UITableView!
+    
+    var reservas: [Reserva] = [
+        Reserva(title: "Restaurante", subtitle: "Menu contemporáneo", dias: "Martes a domingo", horas: "11am a 3pm", image: "img"),
+        Reserva(title: "Visita guiada", subtitle: "Menu contemporáneo", dias: "Martes a domingo", horas: "11am a 3pm", image: "img"),
+        Reserva(title: "Visita guiada para niños ", subtitle: "Menu contemporáneo", dias: "Martes a domingo", horas: "11am a 3pm", image: "img")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupTable()
+    }
+    
+    func setupTable() {
+        reservasTV.delegate = self
+        reservasTV.dataSource = self
+        let nib = UINib(nibName: "RerservasTableViewCell", bundle: nil)
+        reservasTV.register(nib, forCellReuseIdentifier: "reservaCell")
+    }
+
+}
+
+extension ReservasViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        reservas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reservaCell") as! RerservasTableViewCell
+        
+        let reserva = reservas[indexPath.row]
+        cell.titulo.text = reserva.title
+        cell.subtitulo.text = reserva.subtitle
+        cell.dias.text = reserva.dias
+        cell.horas.text = reserva.horas
+        cell.imagen.image = UIImage(named: reserva.image ?? "img")
+        
+        return cell
+    }
+    
+    
+}
---
+++ MuseoDeTodos/MuseoDeTodos/Models/Reserva.swift
@@ -0,0 +1,17 @@
+//
+//  Reserva.swift
+//  MuseoDeTodos
+//
+//  Created by Abraham Cepeda Oseguera on 09/09/21.
+//
+
+import Foundation
+
+
+struct Reserva {
+    let title: String
+    let subtitle: String
+    let dias: String
+    let horas: String
+    let image: String?
+}
