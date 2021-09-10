36542e252e1026679aa062ce35bcb9468df97b8b
LunaAngel17 <70904102+LunaAngel17@users.noreply.github.com>
2021-09-09 17:13:04 -0700

    FetchData Firestore
    Exposiciones firestore link.

--- MuseoDeTodos/MuseoDeTodos/Controllers/ExposicionesViewController.swift
+++ MuseoDeTodos/MuseoDeTodos/Controllers/ExposicionesViewController.swift
@@ -6,38 +6,77 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ExposicionesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //var exposiciones: [Exposicion] = [Exposicion(title: "titulo 1", subtitle: "subtitulo 1", firstDate: "16 OCT 2020", lastDate: "11 ABR 2021", image: ""), Exposicion(title: "titulo 2", subtitle: "subtitulo 2", firstDate: "19 AUG 2021", lastDate: "19 NOV 2021", image: "")]
+        
     
-
     @IBOutlet weak var exposicionesTV: UITableView!
     
-    let exposiciones: [Exposicion] = [
-        Exposicion(title: "titulo 1", subtitle: "subtitulo 1", firstDate: "16 OCT 2020", lastDate: "11 ABR 2021", image: ""),
-        Exposicion(title: "titulo 2", subtitle: "subtitulo 2", firstDate: "19 AUG 2021", lastDate: "19 NOV 2021", image: ""),
-        Exposicion(title: "titulo 3", subtitle: "subtitulo 3", firstDate: "08 SEP 2021", lastDate: "19 OCT 2021", image: "")
-    ]
+    var exposiciones = [Exposicion]()
+    
+    private var db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // set up table view
+        //getData()
+        
+        // set up }table view
         exposicionesTV.delegate = self
         exposicionesTV.dataSource = self
         let nib = UINib(nibName: "ExposicionTableViewCell", bundle: nil)
         exposicionesTV.register(nib, forCellReuseIdentifier: "exposicionCell")
         
-        
+        fetchData()
     }
     
-
+    func fetchData() {
+        db.collection("exposicion").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.exposiciones = documents.compactMap { (queryDocumentSnapshot) -> Exposicion in
+                let data = queryDocumentSnapshot.data()
+                
+                let title = data["titulo"] as? String ?? ""
+                let subtitle = data["subtitulo"] as? String ?? ""
+                let firstDate = data["fechaInicial"] as? String ?? ""
+                let lastDate = data["fechaFinal"] as? String ?? ""
+                let image = ""
+                
+                print(title)
+                return Exposicion(title: title, subtitle: subtitle, firstDate: firstDate, lastDate: lastDate, image: image)
+                
+            }
+            self.exposicionesTV.reloadData()
+        }
+    }
+    
+    //func getData(){
+    //    var exposicionesTemp: [Exposicion] = []
+    //    db.collection("exposicion").getDocuments() { (querySnapshot, err) in
+    //        if let err = err {
+    //            print("Error getting documents: \(err)")
+    //        } else {
+    //            for document in querySnapshot!.documents {
+    //                let tituloTemp : String = document.get("titulo") as! String
+    //               exposicionesTemp.append(Exposicion(title: tituloTemp, subtitle: "subtitulo 1", firstDate: "16 OCT 2020", lastDate: "11 ABR 2021", image: ""))
+    //            }
+    //            dump(exposicionesTemp)
+    //        }
+    //    }
+    //}
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        exposiciones.count
+        return exposiciones.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         //create cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "exposicionCell") as! ExposicionTableViewCell
         
