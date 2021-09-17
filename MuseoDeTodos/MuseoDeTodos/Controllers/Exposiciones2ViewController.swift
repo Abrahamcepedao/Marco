59b3c31d562e3db7b1edcde3c7e4651d5e9db21c
LunaAngel17 <70904102+LunaAngel17@users.noreply.github.com>
2021-09-17 16:39:24 -0700

    Second View Exposiciones
    Se puede dar select a las rows del table view y moverse a una segunda pantalla.

---
+++ MuseoDeTodos/MuseoDeTodos/Controllers/Exposiciones2ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Exposiciones2ViewController.swift
+//  MuseoDeTodos
+//
+//  Created by user193339 on 9/17/21.
+//
+
+import UIKit
+
+class Exposiciones2ViewController: UIViewController {
+
+    @IBOutlet weak var lbl_Title: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
