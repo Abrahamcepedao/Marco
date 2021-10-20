ffed1a3ba5689a8d54bce2a94f7dbc9370081728
Abrahamcepedao <a00827666@itesm.mx>
2021-10-20 12:55:23 -0500

    Updated pagos view controller

---
+++ MuseoDeTodos/MuseoDeTodos/Controllers/AgregarPagoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AgregarPagoViewController.swift
+//  MuseoDeTodos
+//
+//  Created by Abraham Cepeda Oseguera on 20/10/21.
+//
+
+import UIKit
+
+class AgregarPagoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MuseoDeTodos/MuseoDeTodos/Models/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  MuseoDeTodos
+//
+//  Created by Abraham Cepeda Oseguera on 20/10/21.
+//
+
+import Foundation
+
+struct Card {
+    var image: String
+    var number: String
+    var expiration: String
+    var cvc: Int16
+}
